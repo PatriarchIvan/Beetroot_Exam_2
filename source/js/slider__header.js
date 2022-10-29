82106736dcbe676798ecd76144481224ff4f7102
Eugene <edukalis@gmail.com>
2022-10-29 16:12:10 +0300

    completed slider.dots left

---
+++ source/js/slider__header.js
@@ -0,0 +1,81 @@
+const menu = ['', '', '', '', ''];
+const swiper = new Swiper('.header__swiper', {
+  direction: 'vertical',
+  loop: true,
+  effect: 'fade',
+  autoplay: {
+    autoplay: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  pagination: {
+    enabled: true,
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (menu[index]) + '</span>';
+    },
+  },
+
+  on: {
+    init: function () {
+      console.log('swiper initialized');
+    },
+    // transitionEnd: function () {
+      // console.log(this.activeIndex);
+      // for(let i = this.activeIndex; i <= 6; i++) {
+        
+      // }
+      // let pseudo = window.getComputedStyle(document.querySelector('.swiper-slide'), ':after');
+      //   pseudo.getPropertyValue('background');
+      //   let pseudoBackground = pseudo.getPropertyValue('background');
+      //   pseudo.setProperty('background', 'red');
+        
+      // if (this.activeIndex >= 2) {
+      //   console.log('HIT');
+      // }
+      // console.log(`PSEUDO::: ${pseudo}`);
+      // console.log(pseudoBackground);
+      // pseudoBackground = bgColor[2];
+      // console.log(pseudoBackground);
+      // console.log(pseudoBackground);
+      // switch (this.activeIndex) {
+      //   case 2:
+      //     console.log(pseudoBackground);
+      //     pseudoBackground = bgColor[2];
+      //     console.log(pseudoBackground);
+      //     break;
+      //   case 3:
+      //     console.log(pseudoBackground);
+      //     pseudoBackground = bgColor[1];
+      //     console.log(pseudoBackground);
+      //     break;
+      //   case 4:
+      //     pseudoBackground = bgColor[2];
+      //     break;
+      //   case 5:
+      //     pseudoBackground = bgColor[3];
+      //     break;
+      //   case 6:
+      //     pseudoBackground = bgColor[4];
+      //     break;
+      //   default:
+          //     pseudoBackground = bgColor[0];
+          // }
+          // console.log(pseudo.background);
+
+    //   }
+    // },
+  }
+
+});
+
+// document.querySelectorAll(".swiper-slide").forEach(function (d, i) {
+//   d.style.background = bgColor[i];
+// });
