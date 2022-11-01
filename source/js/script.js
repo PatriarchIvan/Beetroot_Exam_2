7ff144de4c1c9af19af2f12ff9109474b244b2f6
Eugene <edukalis@gmail.com>
2022-11-01 11:44:18 +0200

    need to refactot function of burger menu.too much code

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,32 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
-const burgerBtn = document.querySelector('.svg__burger');
-const burgerMenu = document.querySelector('.nav__items');
-// burger init
-window.addEventListener('resize', () => {
-    if(document.documentElement.clientWidth <= 920 || document.documentElement.clientWidth === 920) {
-    burgerBtn.classList.add('animate__animated','animate__bounceInDown');
+    const burgerBtn = document.querySelector('.svg__burger');
+    const burgerMenu = document.querySelector('.nav__items');
+    // burger init
+    // $(burgerMenu).addClass('burger__mode');
+    // $(burgerMenu).addClass('burger__mode');
 
-    } else {
-    burgerBtn.classList.remove('animate__animated','animate__bounceInDown');
-    }
-});
+    $(burgerMenu).addClass('burger__mode');
+    
+    
+    window.addEventListener('resize', () => {
+        if (document.documentElement.clientWidth <= 920) {
+            burgerBtn.classList.add('animate__animated', 'animate__bounceInDown');
+            $(burgerMenu).removeClass('nav__items');
+            $(burgerMenu).hide();
+        } else {
+            burgerBtn.classList.remove('animate__animated', 'animate__bounceInDown');
+            $(burgerMenu).removeClass('burger__mode');
+            $(burgerMenu).addClass('nav__items');
+            $(burgerMenu).show();
+        }
+    });
 
-burgerBtn.addEventListener('click', () => {
-    burgerMenu.classList.add('burger__mode');
-    burgerMenu.style.top = 70 + 'px';
-
-});
-
-
-
+    $(burgerBtn).click(() => {
+        $(burgerMenu).removeClass('nav__items');
+        $(burgerMenu).addClass('animate__animated');
+        $(burgerMenu).addClass('animate__backInLeft');
+        $(burgerMenu).toggle();
+    });
 
 });
