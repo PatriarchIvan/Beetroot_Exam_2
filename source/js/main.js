06376ac86b0a6a259bc5a93fb5c4914b10305e54
Eugene <edukalis@gmail.com>
2022-11-02 13:57:02 +0200

    completed first sec in a sh**ty way.need to move further

--- source/js/main.js
+++ source/js/main.js
@@ -1,28 +1,50 @@
 'use strict';
 const btnDown = document.querySelector('.btn__scroll');
 const leftImg = document.querySelector('.product__bg.img');
-const btnShow = document.querySelector('.text__btn_left');
-const hiddenText = document.querySelector('.text__text_hidden');
+const rightImg = document.querySelector('.product__bg.img_reversed');
+const btnShowLeft = document.querySelector('.text__btn_left');
+const btnShowRight = document.querySelector('.text__btn_right');
+const hiddenTextLeft = document.querySelector('.text__text_hidden');
+const hiddenTextRight = document.querySelector('.text__text_hidden-reversed');
 
 btnDown.addEventListener('click', () => {
     window.scrollTo({
         top: 900,
         behavior: 'smooth',
-    });    
+    });
 });
 
-btnShow.addEventListener('click', () => {
-    hiddenText.style.visibility = 'visible';
-    hiddenText.style.height = 'inherit';
-    leftImg.classList.toggle('animate__left');
-    btnShow.classList.add('animate__animated', 'animate__bounceOutRight');
+btnShowLeft.addEventListener('click', () => {
+    hiddenTextLeft.style.visibility = 'visible';
+    hiddenTextLeft.style.height = 'inherit';
+    hiddenTextLeft.classList.add('animate__animated', 'animate__bounceInRight');
+    btnShowLeft.classList.add('animate__animated', 'animate__bounceOutRight');
     setTimeout(() => {
-        leftImg.classList.toggle('animate__right');
-    }, 4000);
-     setTimeout(() => {
+        btnShowLeft.style.display = 'none';
+    }, 1000);
+
+    if (document.documentElement.clientWidth >= 800) {
         leftImg.classList.toggle('animate__left');
-        leftImg.classList.toggle('animate__right');
-        console.log(leftImg.classList);
-    }, 8010);
+        setTimeout(() => {
+            leftImg.classList.toggle('animate__right');
+        }, 4000);
+    }
 });
 
+btnShowRight.addEventListener('click', () => {
+    hiddenTextRight.style.visibility = 'visible';
+    hiddenTextRight.style.height = 'inherit';
+    hiddenTextRight.classList.add('animate__animated', 'animate__bounceInRight');
+    btnShowRight.classList.add('animate__animated', 'animate__bounceOutLeft');
+    setTimeout(() => {
+        btnShowRight.style.display = 'none';
+    }, 1000);
+
+    if (document.documentElement.clientWidth >= 800) {
+        rightImg.classList.toggle('animate__left_reversed');
+        setTimeout(() => {
+            rightImg.classList.toggle('animate__right_reversed');
+        }, 4000);
+    }
+
+});
