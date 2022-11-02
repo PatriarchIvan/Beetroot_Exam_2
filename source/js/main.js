7f96073c74c7fed8aa11187d90d0f878eaaecb79
Eugene <edukalis@gmail.com>
2022-11-02 10:46:06 +0200

    almost completed first sec

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 const btnDown = document.querySelector('.btn__scroll');
 const leftImg = document.querySelector('.product__bg.img');
-const btnShow = document.querySelector('.text__btn');
-// console.log(leftImg);
+const btnShow = document.querySelector('.text__btn_left');
+const hiddenText = document.querySelector('.text__text_hidden');
 
 btnDown.addEventListener('click', () => {
     window.scrollTo({
@@ -12,17 +12,17 @@ btnDown.addEventListener('click', () => {
 });
 
 btnShow.addEventListener('click', () => {
+    hiddenText.style.visibility = 'visible';
+    hiddenText.style.height = 'inherit';
     leftImg.classList.toggle('animate__left');
+    btnShow.classList.add('animate__animated', 'animate__bounceOutRight');
     setTimeout(() => {
         leftImg.classList.toggle('animate__right');
-        console.log('right');
-    }, 5000),
-    //  setTimeout(() => {
-    //     leftImg.classList.toggle('animate__left');
-    //     leftImg.classList.toggle('animate__right');
-    //     console.log('left' + `${date.getSeconds - (new Date().getSeconds())} sc`);
-    // }, 10010);
-   
-
+    }, 4000);
+     setTimeout(() => {
+        leftImg.classList.toggle('animate__left');
+        leftImg.classList.toggle('animate__right');
+        console.log(leftImg.classList);
+    }, 8010);
 });
 
