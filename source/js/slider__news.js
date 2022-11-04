61d5bbe2cac6d15ae1883bded750c59eaee19bb5
Eugene <edukalis@gmail.com>
2022-11-04 12:23:41 +0200

    ooohh yeah,completed second slider,with other plugin

--- source/js/slider__news.js
+++ source/js/slider__news.js
@@ -1,22 +1,36 @@
-const swiperNews = new Swiper('.swiper__news', {
-    // Optional parameters
-    // direction: 'horizontal',
-    slidesPerView: 3,
-    // slidesPerGroup: 3,
-    spaceBetween: 30,
-    loop: true,
+// const menuNews = ['', '', '', '', ''];
+// const swiperNews = new Swiper('.swiper__news', {
+//     // Optional parameters
+//     // direction: 'horizontal',
+//     slidesPerView: 3,
+//     // slidesPerGroup: 3,
+//     spaceBetween: 30,
+//     loop: true,
+//     autoplay: true,
+
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination-news',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+
+//   });
+//   console.log('ready');
+
+$(document).ready(function () {
+  $('.news__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
     autoplay: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination-news',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
   });
-  console.log('ready');
+});
